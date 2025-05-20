c0634c613eb89593a1333a7273dc161fa937d70e
Fábio Sartori <fabio@fabiosartori.info>
2025-05-20 11:25:02 -0300

    Initial commit

---
+++ test/cdk-lambdas-single-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdasSingleStack from '../lib/cdk-lambdas-single-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambdas-single-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdasSingleStack.CdkLambdasSingleStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
